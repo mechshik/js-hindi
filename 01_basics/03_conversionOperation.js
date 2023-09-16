fa8b07f7dc759aa40ed94aa0a025071a8f89cd14
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-09-16 15:14:37 +0530

    Operation
    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -47,5 +47,47 @@ let booleanIsLoggedIn = Boolean(isLoggenIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+//********************************** OPERATIONS ******************
+
+let value = 3
+let nagValue = -value
+// console.log(nagValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 ="hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+ 2+2);
+// console.log(1+ 2+"2");
+
+console.log(+true);
+console.log(+"")
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
