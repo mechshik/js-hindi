e156a2fc005c985d46375d59fa4dab90682a8aa7
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-09-16 15:00:43 +0530

    Create 03_conversionOperation.js
    Conversion and Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// node 01_basics/03_conversionOperation.js
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+/*
+if null
+object
+object
+number
+0
+*/
+
+/*
+if undefined
+undefined
+undefined
+number
+NaN
+*/
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggenIn = 1
+let isLoggenIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false; 
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
