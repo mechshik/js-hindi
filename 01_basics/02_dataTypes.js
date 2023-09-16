84b868d6e65e1588f60d9c976ac64adddeab53a8
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-09-16 14:32:46 +0530

    Create 02_dataTypes.js
    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"Use strict" // treat all JS code as newer version
+// node 01_basics/02_dataTypes.js
+// alert("hello")
+// alert(3 +3)  // we are using nodejs, not browser
+
+console.log(3+3)  // code readibility should be high
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggenIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh" )
+console.log(typeof null ) // object
+console.log(typeof undefined ) // undefined
