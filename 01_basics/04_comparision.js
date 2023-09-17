42d76495c9d88d005c03d81dc194bc24f6dd53b1
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-09-17 07:30:05 +0530

    Update 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// node 01_basics/04_comparision.js
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// avoid these conversion
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null != 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// === strict check
+
+console.log("2" === 2);
