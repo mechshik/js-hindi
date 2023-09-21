ad5afcb46a3c5511bc1f54f6d26ded37aec1690c
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-09-21 19:49:50 +0530

    Update nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,2 +1,34 @@
 // node 05_iterations/nine.js
 
+const myNums = [1,2,3,4,5,6]
+
+// const myTotal = myNums.reduce(function (acc,currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+
+const myTotal = myNums.reduce((acc,currval) => acc+currval,0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName:"js course",
+        price: 999
+    },
+    {
+        itemName:"mobile dev course",
+        price: 5999
+    },
+    {
+        itemName:"data science course",
+        price: 12999
+    }
+]
+
+
+const priceToPay =  shoppingCart.reduce((acc, item ) => acc+ item.price,0)
+
+console.log(priceToPay);
