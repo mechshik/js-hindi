8ae6532785e8d2d30733eb9fd93190dcd70eb50e
Shikhar Kumar Mall <shikharkumarmall@gmail.com>
2023-10-03 11:24:40 +0530

    after change

---
+++ 10_classes_and_oop/practice.js
@@ -0,0 +1,32 @@
+//for
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+//forEach
+array.forEach(element => {
+    
+});
+
+// function
+function name(params) {
+    
+}
+
+// forin
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+
+//forof
+for (const iterator of object) {
+    
+}
+
+
+
